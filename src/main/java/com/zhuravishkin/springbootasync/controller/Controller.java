e13bbde086db2ddf8f1d62d6ecde5bc16aa4f5e1
alexey.zhuravishkin <zhuravishkin@gmail.com>
2020-08-29 18:30:27 +0300

    added rest template async

--- src/main/java/com/zhuravishkin/springbootasync/controller/Controller.java
+++ src/main/java/com/zhuravishkin/springbootasync/controller/Controller.java
@@ -2,6 +2,7 @@ package com.zhuravishkin.springbootasync.controller;
 
 import com.zhuravishkin.springbootasync.model.User;
 import com.zhuravishkin.springbootasync.service.AsyncService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+@Slf4j
 @RestController
 @RequestMapping(value = "async")
 public class Controller {
@@ -22,7 +27,14 @@ public class Controller {
 
     @PostMapping(value = "/post")
     public ResponseEntity<User> post(@RequestBody User user) {
-        service.getAsync();
-        return new ResponseEntity<>(user, HttpStatus.OK);
+        CompletableFuture<User> userCompletableFuture = service.getAsync();
+        User userFuture = null;
+        try {
+            userFuture = userCompletableFuture.get();
+        } catch (InterruptedException | ExecutionException e) {
+            log.error(e.getMessage(), e);
+            Thread.currentThread().interrupt();
+        }
+        return new ResponseEntity<>(userFuture, HttpStatus.OK);
     }
 }
--- src/main/java/com/zhuravishkin/springbootasync/service/AsyncService.java
+++ src/main/java/com/zhuravishkin/springbootasync/service/AsyncService.java
@@ -1,21 +1,39 @@
 package com.zhuravishkin.springbootasync.service;
 
+import com.zhuravishkin.springbootasync.model.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @Async
 @Service
 public class AsyncService {
-    public void getAsync() {
+    private final RestTemplate restTemplate;
+
+    public AsyncService(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder
+                .setConnectTimeout(Duration.ofSeconds(5))
+                .setReadTimeout(Duration.ofSeconds(5))
+                .build();
+    }
+
+    public CompletableFuture<User> getAsync() {
         log.info("Async start");
+        User user = null;
         try {
+            user = restTemplate.getForObject("http://localhost:8080/template/get", User.class);
             Thread.sleep(10000);
         } catch (InterruptedException e) {
             log.error(e.getMessage(), e);
             Thread.currentThread().interrupt();
         }
         log.info("Async end");
+        return CompletableFuture.completedFuture(user);
     }
 }
