c74ee318aeed7268b5ef137dbb8c46581d9bd0f6
alexey.zhuravishkin <zhuravishkin@gmail.com>
2021-02-22 17:21:21 +0300

    refactored multiple async

---
+++ src/main/java/com/zhuravishkin/springbootasync/service/AsyncCachedService.java
@@ -0,0 +1,34 @@
+package com.zhuravishkin.springbootasync.service;
+
+import com.zhuravishkin.springbootasync.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+@Service
+public class AsyncCachedService {
+    private final RestTemplate restTemplate;
+
+    public AsyncCachedService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Async("cachedThreadPoolTaskExecutor")
+    public CompletableFuture<User> getCachedAsync() {
+        log.info("async cached start");
+        User user = null;
+        try {
+            user = restTemplate.getForObject("http://localhost:8080/template/get", User.class);
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            log.error(e.getMessage(), e);
+            Thread.currentThread().interrupt();
+        }
+        log.info("async cached end");
+        return CompletableFuture.completedFuture(user);
+    }
+}
--- src/main/java/com/zhuravishkin/springbootasync/service/AsyncService.java
+++ src/main/java/com/zhuravishkin/springbootasync/service/AsyncService.java
@@ -2,12 +2,10 @@ package com.zhuravishkin.springbootasync.service;
 
 import com.zhuravishkin.springbootasync.model.User;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
@@ -15,19 +13,18 @@ import java.util.concurrent.ExecutionException;
 @Service
 public class AsyncService {
     private final RestTemplate restTemplate;
+    private final AsyncCachedService asyncCachedService;
 
-    public AsyncService(RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplate = restTemplateBuilder
-                .setConnectTimeout(Duration.ofSeconds(5))
-                .setReadTimeout(Duration.ofSeconds(5))
-                .build();
+    public AsyncService(RestTemplate restTemplate, AsyncCachedService asyncCachedService) {
+        this.restTemplate = restTemplate;
+        this.asyncCachedService = asyncCachedService;
     }
 
     @Async("threadPoolTaskExecutor")
     public CompletableFuture<User> getAsync() {
         log.info("async fixed start");
         User user = null;
-        CompletableFuture<User> userCompletableFuture = getCachedAsync();
+        CompletableFuture<User> userCompletableFuture = asyncCachedService.getCachedAsync();
         try {
             user = restTemplate.getForObject("http://localhost:8080/template/get", User.class);
             Thread.sleep(10000);
@@ -42,21 +39,6 @@ public class AsyncService {
             Thread.currentThread().interrupt();
         }
         log.info("async fixed end");
-        return CompletableFuture.completedFuture(user);
-    }
-
-    @Async("cachedThreadPoolTaskExecutor")
-    public CompletableFuture<User> getCachedAsync() {
-        log.info("async cached start");
-        User user = null;
-        try {
-            user = restTemplate.getForObject("http://localhost:8080/template/get", User.class);
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            log.error(e.getMessage(), e);
-            Thread.currentThread().interrupt();
-        }
-        log.info("async cached end");
         return CompletableFuture.completedFuture(user);
     }
 }
